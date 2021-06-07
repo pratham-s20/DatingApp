8cc74b3dcf1428e54fd7a665a3a12b507592d30d
Prathamesh <pratham.s20@gmail.com>
2021-06-07 14:36:52 -0300

    End of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using API.Data;
+using API.Entities;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {        
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return _context.Users.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUsers(int id)
+    {
+        return _context.Users.Find(id);
+    }
+    }
+}
